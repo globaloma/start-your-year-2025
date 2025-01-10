5c639cd751b54e08ebc84f08381a1ad431ffebe1
Chioma <107587589+globaloma@users.noreply.github.com>
2025-01-10 09:08:26 +0000

    questone complete

---
+++ javascript/data-types.js
@@ -0,0 +1,23 @@
+let variable = "foo";  // this is a string
+variable = 0;  // variable is reassigned as a number
+variable = false;  // variable is reassigned as a boolean
+
+let foo = variable + 12;
+foo = foo + " this is twelve and ";
+foo = foo + 0;
+console.log(foo);
+
+
+function addATrueNumber(x, y) {
+    if ((typeof x === "number") && (typeof y === "number")) {
+      return x + y;
+    }
+    throw new Error("One of the inputs is not a number", {
+      cause: "Not a number.",
+    });
+  }
+  
+  const aTrueNumber = addATrueNumber(12, 12);
+  console.log(aTrueNumber);
+
+  const notATrueNumber = addATrueNumber(12, "12");
